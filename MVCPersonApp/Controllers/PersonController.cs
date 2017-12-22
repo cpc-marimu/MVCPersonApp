2dc25150f1894a84925aa57a541b6989b5867331
umchandr <umchandr@gmail.com>
2017-12-22 10:15:56 -0600

    MVC Person

---
+++ MVCPersonApp/Controllers/PersonController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using MVCPersonApp.Models;
+namespace MVCPersonApp.Controllers
+{
+    public class PersonController : Controller
+    {
+        IList<PersonModel> personList = new List<PersonModel>() {
+                    new PersonModel(){ Id=1, FirstName="John", LastName = "Smith" }
+                };
+
+        // GET List: Person
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        // GET Details: Person
+        public ActionResult Details()
+        {
+            if (Session["Person"] != null)
+            {
+                PersonModel model = (PersonModel)Session["Person"];
+                return View(model);
+            }
+            else
+            {
+                var persn = personList.Where(s => s.Id == 1).FirstOrDefault();
+                if (persn == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(persn);
+            }
+        }
+
+        public ActionResult Edit(int? PersonId)
+        {
+            if (PersonId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if (Session["Person"] != null) { PersonModel model = (PersonModel)Session["Person"]; return View(model); }
+            else
+            {
+                var persn = personList.Where(s => s.Id == PersonId).FirstOrDefault();
+                if (persn == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(persn);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(PersonModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Session["Person"] = model;
+                return RedirectToAction("Details");
+            }
+            return View(model);
+        }
+
+        public ActionResult Index()
+        {
+            return View(personList);
+        }
+    }
+}
---
+++ MVCPersonApp/Models/PersonModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCPersonApp.Models
+{
+    public class PersonModel
+    {
+        [Required]
+        [Display(Name = "ID")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    }
+}
